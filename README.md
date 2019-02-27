ec56da5008eec62a35774ff5ff7dd7da4120951a
Abhishek Rana <abhrana2@publicisgroupe.net>
2019-02-27 13:29:26 +0530

    Initial setup

---
+++ README.md
@@ -0,0 +1,29 @@
+# NgrxStoreEffectLearning
+
+Learn ngrx-store with ngrx-effects.
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.3.
+
+## Run Application
+
+Run `npm start` for a dev server and mock data server together. The app will automatically open on `http://localhost:9100/` and mock data server will run on `http://localhost:9101/`.
+
+## Development server
+
+Run `npm run serve` for a dev server. Navigate to `http://localhost:9100/`. The app will automatically reload if you change any of the source files.
+
+## Mock data server
+
+Run `npm run mock:server` for a dev server. Navigate to `http://localhost:9101/`. The app will automatically reload if you change any of the source files.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { StoreModule, MetaReducer } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+// not used in production
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { storeFreeze } from 'ngrx-store-freeze';
+
+// this would be done dynamically with webpack for builds
+const environment = {
+  development: true,
+  production: false
+};
+
+export const metaReducers: MetaReducer<any>[] = !environment.production
+  ? [storeFreeze]
+  : [];
+
+// bootstrap
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, { metaReducers }),
+    EffectsModule.forRoot([]),
+    environment.development ? StoreDevtoolsModule.instrument() : []
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
